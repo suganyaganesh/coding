de12dada6830c321fcca899b2f489174d5bfb4a1
suganyaganesh <suganya31893@gmail.com>
2019-01-26 21:54:49 +0530

    add new excercise code

---
+++ home.py
@@ -0,0 +1,49 @@
+#this is add function for additon
+def add():
+ print("from the add function")
+ print("A=")
+ A=input("A is =")
+ print("B=")
+ B=input("B is =")
+ print("A+B is ", int(A)+int(B))
+#add()
+
+#sub function for subtraction
+def sub():
+ print("from sub")
+ print("python code")
+ A=input("A=")
+ B=input("B=")
+ print("the subtraction is A-B =" , int(A)-int(B))
+#sub()
+
+#code for name for using two string in one variable
+def git():
+ print("from function git")
+ username=input()
+ address=input()
+ print("username",username,"\n address",address)
+#git()
+
+#print two string as input
+def hit():
+ firstname=input()
+ secondname=input()
+ print("firstname=",firstname)
+ print("secondname=",secondname)
+#hit()
+
+#multiplication code
+def mul():
+ A=input("the value of A is =")
+ B=input("the value of B is=")
+ print("the multiplication of A and B is =", int(A)*int(B))
+#mul()
+
+#division code
+def div():
+ A=30
+ B=5
+ print("the divident of A and B is:", A/B)
+#div()
+

036f838f9dd8b0250929b1a1dc7f382bf30c98a0
suganyaganesh <suganya31893@gmail.com>
2019-01-26 22:33:04 +0530

    add second coding file

---
+++ homesecond.py
@@ -0,0 +1,60 @@
+# Loops
+def loop():
+ for i in range(7):
+  print("the value of range :", i)
+#loop()
+
+#for loop code
+def user():
+ y=input("the value of y is :")
+ for j in range(int(y)):
+  print("the range of A :", y ,"value of J", j)
+#user()
+
+#if loop code
+def loops():
+ n=input("the value of n :")
+ i=n
+ print
+ if i=="7":
+  print("the count:",n,"the value of i:",i)
+ else:
+  print("error")
+#loops()
+
+# for in if loop
+def cal():
+ sub1=input("the value of sub1 is:")
+ sub2=input("the value of sub2 is:")
+ sub3=input("the value of sub3 is:")
+ sub4=input("the value of sub4 is:")
+ sub5=input("the value of sub5 is:")
+ total=int(sub1)+int(sub2)+int(sub3)+int(sub4)+int(sub5)
+ i=total
+ for i in range(total >=250):
+  if total>=450:
+   print("the value of this total=",total,"passed with distinction!!")
+  elif total>=350:
+   print("the value of this total=",total,"passed with firstclass!")
+  else:
+   print("the value of this total =",total,"passed with second class")
+#cal()
+
+#whileloop
+def whileloop():
+ i=7
+ while i==7:
+  print("the value of =",i)
+  break
+#whileloop()
+
+def forwhile():
+ for i in range(8):
+  print("the value :",i)
+  while(i==2):
+   print("the value in :",i)
+   break
+  if(i==3):
+   print("the value is :",i)
+#forwhile()
+

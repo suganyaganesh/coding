96aa533124b1d264c468f6eaa8de5b878d34c7b3
suganyaganesh <suganya31893@gmail.com>
2019-02-03 13:13:47 +0530

    add comment line on flask excel open file

--- flaskexcel.py
+++ flaskexcel.py
@@ -1,17 +1,21 @@
+# Import Flask module from flask
 from flask import Flask
 
-
+# default arguments for flask in web
 app=Flask(__name__)
 
+# this is route function which / used to route the function in browser
 @app.route('/')
-def home():
- return 'hello world'
+# this is home function for the web Url after route in browser
+def home():  
+ return 'hello world'   # while execution it will return the Hello world
+# this is another routing function which execute on open excel 
 @app.route('/excel')
-def excel():
- import test
- test.os.system('start bar.xlsx')
+def excel(): # this is another function
+ import excelopen        #import excel open function in module
+ excelopen.os.system('start openxlexam.xlsx')   # this is call module for exceution
  return 'hello this is excel'
  
 if __name__=='__main__':
- app.run()
+ app.run()  # call the default app function 
  
